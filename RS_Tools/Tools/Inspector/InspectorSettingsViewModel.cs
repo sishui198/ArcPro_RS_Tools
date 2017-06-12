715eed3078475349472ff91be0790971cb020978
WOOLPERTINC\Stayte <ctstayte@gmail.com>
2017-06-12 13:26:34 -0400

    Version 1.4 Fixed Building Inspector

--- RS_Tools/Tools/Inspector/InspectorSettingsViewModel.cs
+++ RS_Tools/Tools/Inspector/InspectorSettingsViewModel.cs
@@ -453,12 +453,20 @@ namespace RS_Tools.Tools.Inspector
                                 var shape = feature.GetShape() as Geometry;
                                 Envelope envelope = shape.Extent;
                                 envelope.Expand(5, 5, true);
-                                
+
                                 if (!KeepScale) MapView.Active.ZoomTo(envelope, null, false);
                                 else MapView.Active.PanTo(shape); // Okay, Scale implementation
 
-                                queryfilter.WhereClause = "ObjectID = " + feature.GetObjectID().ToString();
-                                _selectedLayer.Select(queryfilter, SelectionCombinationMethod.New);
+                                // Get The Next Feature
+                                IReadOnlyDictionary<MapMember, List<long>> NextFeature = new Dictionary<MapMember, List<long>>
+                                {
+                                    {_selectedLayer, new List<long> { feature.GetObjectID()} }
+                                };
+
+                                _selectedMap.SetSelection(NextFeature, SelectionCombinationMethod.New);
+
+                                //queryfilter.WhereClause = "= " + feature.GetObjectID().ToString();
+                               // _selectedLayer.Select(queryfilter, SelectionCombinationMethod.New);
 
                                 return;
 
